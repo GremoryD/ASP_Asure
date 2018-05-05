61dc3faa4eb6262eea5420a6d37d42e37051425a
GremoryD <dima.lamashevski@gmail.com>
2018-05-06 00:33:57 +0300

    Last version

--- ASP_Azure_test/Pages/Error.cshtml.cs
+++ ASP_Azure_test/Pages/Error.cshtml.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace ASP_Asure.Pages
+namespace ASP_Azure_test.Pages
 {
     public class ErrorModel : PageModel
     {
--- ASP_TEST/Pages/Index.cshtml.cs
+++ ASP_TEST/Pages/Index.cshtml.cs
@@ -1,18 +1,16 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace ASP_Asure.Pages
+namespace ASP_TEST.Pages
 {
-    public class ContactModel : PageModel
+    public class IndexModel : PageModel
     {
-        public string Message { get; set; }
-
         public void OnGet()
         {
-            Message = "Your contact page.";
         }
     }
 }
